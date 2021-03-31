fa41d1b6347c476be9aed2d3b031e92a9d291ace
Saurabh Joshi <joshisaurabh2610@gmail.com>
2021-03-31 13:08:37 +0530

    aweesom side Drawer with awesome hamberger added

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxilliary/Auxilliary'
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-const Layout=(props)=>{
-    return(
-        <Aux>
-            <Toolbar/>
-            <main>
-                {props.children}
-            </main>
-        </Aux>
-    );
+import SideDrawer from '../NavBar/SideDrawer/SideDrawer'
+import Toolbar from '../NavBar/Toolbar/Toolbar'
+import Backdrops from '../UI/Backdrops/Backdrop'
+class Layout extends Component{
+    state={
+        showSideDrawer:false
+    }
+    toggleSideDrawer=()=>{
+        // console.log('show side drawer function invoked');
+        // console.log(this.state.showSideDrawer);
+        this.setState({
+            showSideDrawer:!this.state.showSideDrawer
+        });
+    }
+    render(){
+        return(
+            <Aux>     
+                <Toolbar toggle={this.toggleSideDrawer}
+                        show={this.state.showSideDrawer}/>
+                <SideDrawer toggle={this.toggleSideDrawer}
+                        show={this.state.showSideDrawer}/>
+                <Backdrops 
+                        show={this.state.showSideDrawer}
+                        cancel={this.toggleSideDrawer}/>
+                <main>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
 };
 
 export default Layout;
--- src/Components/NavBar/Toolbar/Toolbar.js
+++ src/Components/NavBar/Toolbar/Toolbar.js
@@ -2,10 +2,13 @@ import classes from './Toolbar.module.css';
 import React from 'react'
 import Logo from '../../Logo/logo'
 import Navitem from '../NavItem/Navitem'
+import Hamberger from '../../HambergerMenu/Hamberger';
 
-const Toolbar=()=>{
+const Toolbar=(props)=>{
     return(
         <div className={classes.Toolbar}>
+            <Hamberger clicked={props.toggle}
+                        open={props.show}/>
             <Logo />
             <div className={classes.Navitems}>
                 <Navitem active link="/">Home</Navitem>
