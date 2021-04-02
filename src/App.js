b6d5e62b2e258c58143fa259eeb2aa11d02b8631
Saurabh Joshi <joshisaurabh2610@gmail.com>
2021-04-02 16:22:25 +0530

    checking code removed

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@ import React,{ Component } from "react";
 import BurgerBuilder from "./Containers/BurgerBuilder/burgerBuilder";
 import Layout from "./Components/Layout/Layout";
 class App extends Component {
-  state={
-    show:true
-  }
-  componentDidMount(){
-    setTimeout(()=>{
-      this.setState({show:false});
-    },5000)
-  }
   render() {
     return (
       <Layout>
-        {this.state.show? <BurgerBuilder/>: null}
+         <BurgerBuilder/>
       </Layout>
     );
   }
